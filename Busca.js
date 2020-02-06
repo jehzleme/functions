b92dbf77454496d09f9a251d7006dc4ca92140d2
Jéssica Leme Ambrust <jehzleme@gmail.com>
2020-02-05 23:45:20 -0300

    Exercício 2 do Desafio01-3 do Bootcamp Launchbase da Rocketseat

--- Busca.js
+++ Busca.js
@@ -13,3 +13,17 @@ const usuarios = [
     }
 ]
 
+function checaSeUsaCSS(usuario) {
+    for (let i=0; i < usuario.tecnologias.length; i++){
+        if(usuario.tecnologias[i] == 'CSS'){
+            return true
+        }
+    }
+}
+
+for (let i=0; i<usuarios.length; i++){
+    const usuarioTrabalhaComCSS =checaSeUsaCSS(usuarios[i]);
+    if (usuarioTrabalhaComCSS){
+        console.log (`O usuário ${usuarios[i].nome} trabalha com CSS`)
+    }
+}
